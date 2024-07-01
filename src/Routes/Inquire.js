83acd79fe8d6a6043da299a9a26e59972c4fd8a4
PanitanK <98577467+PanitanK@users.noreply.github.com>
2024-07-01 10:21:53 +0700

    Updated 1/7 10.20

--- src/Routes/Inquire.js
+++ src/Routes/Inquire.js
@@ -5,51 +5,69 @@ import { storage, db } from './Firebase';
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { doc, setDoc } from 'firebase/firestore';
 import { useNavigate } from "react-router-dom";
-import Title from './image/logo/Techbite1.png';
+import Title from './image/logo/DemoDay.png';
 import UploadIcon from './image/svg/Upload-Icon.png'; // Replace with your image path
+import AlertBox from './Component/AlertBox'; // Import the AlertBox component
 
 function Inquire() {
-  const navigate = useNavigate();
   const [email, setEmail] = useState('');
-  const [profileURL, setProfileURL] = useState('');
+  const [organisation, setOrganisation] = useState('');
+  const [name, setName] = useState('');
+  const [profileURL, setProfileURL] = useState(UploadIcon); // Set initial state to the upload icon
   const fileInputRef = useRef(null); // Reference to the hidden file input
 
-  const handleImageChange = (e) => {
-    if (e.target.files[0]) {
-      handleUpload(e.target.files[0]);
-    }
+  const [alertVisible, setAlertVisible] = useState(false); // State to manage alert visibility
+  const [alertTopic, setAlertTopic] = useState(''); // State to manage alert topic
+  const [alertContent, setAlertContent] = useState(''); // State to manage alert content
+
+  const handleImageClickAndUpload = async (e) => {
+    e.preventDefault();
+    fileInputRef.current.click();
   };
 
-  const handleUpload = async (file) => {
+  const handleImageChange = async (event) => {
+    const file = event.target.files[0];
     if (file) {
       const imageRef = ref(storage, `profilePictures/${file.name}`);
       await uploadBytes(imageRef, file);
       const downloadURL = await getDownloadURL(imageRef);
 
-      // Store the URL in Firestore
-      const docRef = doc(db, "Registration", email);
-      await setDoc(docRef, { profilePictureURL: downloadURL }, { merge: true });
-
+      // Update the profile image URL to the uploaded image
       setProfileURL(downloadURL);
-      alert("Profile picture uploaded successfully!");
-
-      // Navigate to main page or perform other actions
-      navigate('/main'); // Adjust the path as needed
     } else {
-      alert("Please select an image to upload.");
+      setAlertTopic('Attention');
+      setAlertContent('Please Select Image');
+      setAlertVisible(true); // Show the alert
     }
   };
 
-  const handleImageClick = () => {
-    fileInputRef.current.click(); // Trigger the file input click
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Store the form data and profile image URL in Firestore
+    const docRef = doc(db, "Registration", email);
+    await setDoc(docRef, {
+      email,
+      organisation,
+      name,
+      profilePictureURL: profileURL
+    }, { merge: true });
+
+    setAlertTopic('Done');
+    setAlertContent('Thank you for your registration. See you on Demo Day!');
+    setAlertVisible(true); // Show the alert
+
+    // Clear the inputs and reset the profile picture
+    setEmail('');
+    setOrganisation('');
+    setName('');
+    setProfileURL(UploadIcon);
   };
 
   return (
     <div className={styles['App-Inquire']}>
-      <div className={styles['App-Inquire-Header']} >
-        <h1>TECHBITE 5.0 Demo Day</h1>
+      <div className={styles['App-Inquire-Header']}>
         <img src={Title} alt="Welcome" className="TECHBITE_CLUB" />
-        <h4>TECHBITE CLUB REGISTRATION</h4>
+        <h1>TECHBITE CLUB REGISTRATION</h1>
       </div>
 
       <div className={styles['Input-Box']}>
@@ -61,22 +79,27 @@ function Inquire() {
       </div>
 
       <div className={styles['Reg-Box']}>
-        <form onSubmit={(e) => e.preventDefault()}>
-        <div className={styles["form-style-box-img"]}>
+        <form onSubmit={handleSubmit}>
+          <div className={styles["form-style-box-img"]}>
             <h5 htmlFor="Image" className={styles["input-placeholder"]}>Profile Picture</h5>
-            <div className={styles["custom-file-input-wrapper"]} onClick={handleImageClick}>
+            <div
+              className={`${styles["custom-file-input-wrapper"]} ${profileURL === UploadIcon ? styles["upload-icon"] : styles["uploaded-img"]}`}
+              onClick={handleImageClickAndUpload}
+            >
               <img
-                src={UploadIcon} // Use your own image for upload button
+                src={profileURL} // Display the uploaded image or upload icon
                 alt="Upload"
                 className={styles["custom-file-input-img"]}
               />
             </div>
-            <div className= {styles["Label-IMG-Container"]}>
-              <p>If none were selected Company banner will be used as default</p>
-            </div>
-           
+            {!profileURL.includes('firebase') && (
+              <div className={styles["Label-IMG-Container"]}>
+                <p>If none were selected, the company banner will be used as default</p>
+              </div>
+            )}
             <input
               type="file"
+              accept="image/*"
               id="Image"
               className={styles["hidden-file-input"]}
               ref={fileInputRef}
@@ -84,7 +107,7 @@ function Inquire() {
               required
             />
           </div>
-          
+
           <div className={styles["form-style-box"]}>
             <h5>Email</h5>
             <input
@@ -100,11 +123,11 @@ function Inquire() {
           <div className={styles["form-style-box"]}>
             <h5>Organisation</h5>
             <input
-              type="email"
-              id="Email"
+              type="text"
+              id="Organisation"
               className={styles["form-style"]}
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              value={organisation}
+              onChange={(e) => setOrganisation(e.target.value)}
               required
             />
           </div>
@@ -112,22 +135,27 @@ function Inquire() {
           <div className={styles["form-style-box"]}>
             <h5>Name</h5>
             <input
-              type="email"
-              id="Email"
+              type="text"
+              id="Name"
               className={styles["form-style"]}
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               required
             />
           </div>
-
-          
 
           <div>
             <button type="submit" className={styles["submit-button"]}>Submit</button>
           </div>
         </form>
       </div>
+      {alertVisible && (
+        <AlertBox
+          alertTopic={alertTopic}
+          alertContent={alertContent}
+          onClose={() => setAlertVisible(false)} // Close the alert
+        />
+      )}
     </div>
   );
 }
